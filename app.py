69cc224afd885c19ba9ab9044d76abbe263173d3
Açucena Soares <acucenarodrigues1998@gmail.com>
2024-04-22 14:40:08 -0300

    Criando uma estrutura de páginas e subindo uma versão para deploy no streamlit community

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from utils import get_tools_list, create_agent, parse_refs
+from scripts.utils import get_tools_list, create_agent, parse_refs
 
 st.title('ResearchBuddy')
+
+gemini_api_key = st.sidebar.text_input('Gemini API Key', type='password')
+serpapi_api_key = st.sidebar.text_input('SerpAPI API Key', type='password')
+
+start_message = """
+Hi, I'm ResearchBuddy! I use Google's Gemini model to help you with your literature search.
+
+To work, I need you to add the API keys requested in the sidebar. 
+
+I use these APIs to:
+* Collect article data directly from Google Scholar to answer your questions.
+* Formulate a user-friendly answer to your question.
+
+To obtain the API keys, go to the following links:
+
+[Gemini](https://ai.google.dev/)
+
+[SerpAPI](https://serpapi.com/users/sign_up)
+
+"""
+
+with st.chat_message("assistant"):
+    st.markdown(start_message)
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -16,26 +39,31 @@ for message in st.session_state.messages:
 
 query = st.chat_input("Ask your question here...")
 
-if query:
-    with st.chat_message("user"):
-        st.markdown(query)
+if gemini_api_key and serpapi_api_key:
 
-    try:
-        st.session_state.messages.append({"role": "user", "content": query})
-        
-        tools = get_tools_list()
-        agent = create_agent(tools)
-        response = agent.chat(query)
-        papers = parse_refs(response.sources)
+    if query:
+        with st.chat_message("user"):
+            st.markdown(query)
 
-        with st.chat_message("assistant"):
-            st.markdown(str(response))
-        st.session_state.messages.append({"role": "assistant", "content": str(response)})
+        try:
+            st.session_state.messages.append({"role": "user", "content": query})
+            
+            tools = get_tools_list(serpapi_api_key, gemini_api_key)
+            agent = create_agent(tools, gemini_api_key)
+            response = agent.chat(query)
+            papers = parse_refs(response.sources)
 
-        with st.chat_message("assistant"):
-            st.markdown(papers)
-        st.session_state.messages.append({"role": "assistant", "content": papers})
-    except:
-        with st.chat_message("assistant"):
-            st.markdown("I couldn't find any papers related to your question. Try writing it in a different way.")
+            with st.chat_message("assistant"):
+                st.markdown(str(response))
+            st.session_state.messages.append({"role": "assistant", "content": str(response)})
 
+            with st.chat_message("assistant"):
+                st.markdown(papers)
+            st.session_state.messages.append({"role": "assistant", "content": papers})
+        except:
+            with st.chat_message("assistant"):
+                st.markdown("I couldn't find any papers related to your question. Try writing it in a different way.")
+
+else:
+    with st.chat_message("assistant"):
+        st.markdown("Please add the requested API keys to the sidebar.")
--- scripts/utils.py
+++ scripts/utils.py
@@ -2,34 +2,38 @@ import yaml
 from llama_index.llms.gemini import Gemini
 from llama_index.core import ServiceContext
 from llama_index.embeddings.gemini import GeminiEmbedding
-from google_scholar_tool import GoogleScholarToolSpec
-from main_theme_extractor_tool import ThemeExtractorTool
+from scripts.google_scholar_tool import GoogleScholarToolSpec
+from scripts.main_theme_extractor_tool import ThemeExtractorTool
 from llama_index.core.agent import ReActAgent
 from llama_index.core.memory import ChatMemoryBuffer
 
 memory = ChatMemoryBuffer.from_defaults(token_limit=3900)
 
 # Carregar o arquivo YAML
-with open('credentials.yaml', 'r') as file:
+"""with open('credentials.yaml', 'r') as file:
     credenciais = yaml.safe_load(file)
 # Acessar as credenciais
 gemini_api_key = credenciais['gemini_api_key']
 serpapi_api_key = credenciais['serpapi_api_key']
+"""
 
-llm=Gemini(model_name="models/gemini-1.0-pro-001", api_key=gemini_api_key)
-service_context = ServiceContext.from_defaults(
-    llm=llm,
-    embed_model=GeminiEmbedding(model_name="models/embedding-001", api_key=gemini_api_key)
-)
+def gen_llm(llm_api_key):
+    llm=Gemini(model_name="models/gemini-1.0-pro-001", api_key=llm_api_key)
+    service_context = ServiceContext.from_defaults(
+        llm=llm,
+        embed_model=GeminiEmbedding(model_name="models/embedding-001", api_key=llm_api_key)
+    )
+    return llm
 
-def get_tools_list():
+def get_tools_list(serpapi_api_key, llm_api_key):
     scholar_tool = GoogleScholarToolSpec(serpapi_api_key).to_tool_list()
+    llm = gen_llm(llm_api_key)
     query_key = ThemeExtractorTool(llm).to_tool_list()
 
     tools = scholar_tool + query_key
     return tools
 
-def create_agent(tools):
+def create_agent(tools, llm_api_key):
     context = """
     You are responsible for summarizing the bibliographical references for a search. \n
     You will be given a question and must use the tools available to find the central theme \n
@@ -39,6 +43,8 @@ def create_agent(tools):
     * The answer to the question asked by the user\n
     * And the references of all the articles found, including names, authors and access links
     """
+
+    llm = gen_llm(llm_api_key)
 
     agent = ReActAgent.from_tools(
         tools,
